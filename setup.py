725641662cb5f65b874f5519d50d7f6a7e47069a
Kensuke Saito <ken.saito.0813@gmail.com>
2023-03-08 22:06:06 +0900

    Removed unnecessary files

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
         name="ezpylogger",
-        version="0.1.1",
+        version="0.1.2",
         author="Kensuke Saito",
         author_email="bebf.heavysec@gmail.com",
         description="ezpylogger: Log easier and simpler",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/bonohub13/pylog.git",
+        url="https://github.com/bonohub13/ezpylogger.git",
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
